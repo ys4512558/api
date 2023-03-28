4a276e17efb4327f56270f71c416865a946b7b6e
Beudicri <ys4512558@naver.com>
2023-03-28 22:06:03 +0900

    Get_api

--- api/src/main/java/com/springboot/api/controller/GetController.java
+++ api/src/main/java/com/springboot/api/controller/GetController.java
@@ -1,11 +1,64 @@
 package com.springboot.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class GetController {
     //http://localhost:8080/api/v1/get-api/hello
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getHello(){
+        return "Hello World";
+    }
+
+    //매개변수가 없는 Get방식
+    //http://localhost:8080/api/v1/get-api/name
+    @GetMapping(value = "/name")
+    public String getName(){
+        return "Flature";
+    }
+    
+    //매개변수를 받는 Get방식
+    //http://localhost:8080/api/v1/get-api/variable1/{String 값}
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable){
+        return variable;
+    }
+
+    //매개변수를 받는 Get방식 + GetMapping의 변수를 다른 변수로 매핑하는 방법
+    //http://localhost:8080/api/v1/get-api/variable2/{String 값}
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var){
+        return var;
+    }
+
+    //@RequestParam을 이용하여 url뒤의 값을 그대로 가져옴
+    //http://localhost:8080/api/v1/get-api/request1?name=value1&email=value2&organization=value3
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization){
+
+        return name + " " + email + " " + organization;
+    }
+
+    //쿼리스트링에 어떤 값이 들어올지 모르는 상황에는 Map객체를 활용할 수 있음
+    //http://localhost:8080/api/v1/get-api/request2?key1=value1&key2=value2
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(
+            @RequestParam Map<String, String> param){
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+
 
 }
