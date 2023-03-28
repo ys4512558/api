e2cab3954a12ea8d571464f94a18b6b139c9f40d
Beudicri <ys4512558@naver.com>
2023-03-28 19:11:14 +0900

    Initial commit

---
+++ api/src/main/java/com/springboot/api/controller/GetController.java
@@ -0,0 +1,11 @@
+package com.springboot.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+    //http://localhost:8080/api/v1/get-api/hello
+
+}
